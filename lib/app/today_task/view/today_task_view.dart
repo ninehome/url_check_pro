a3525156cf540935c9408997e64742951b2c05a9
let.s go <113565560+ninehome@users.noreply.github.com>
2025-08-30 18:16:21 +0800

    x
    x

--- lib/app/today_task/view/today_task_view.dart
+++ lib/app/today_task/view/today_task_view.dart
@@ -13,80 +13,137 @@ class TodayTaskView extends GetView<TodayTaskController>{
         color:  const Color(0xffffffff),
       ),
       margin: EdgeInsets.only(left: 20.w,right: 20.w,bottom: 20.w),
-      child: ListView.builder(
-          itemCount: 10,
-          padding: EdgeInsets.zero,
-          itemBuilder: (ctx,index){
-            return Container(
-              width: double.infinity,
-              margin: EdgeInsets.only(left: 15,right: 15),
-              height: 60,
-              child: Stack(
-                children: [
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20.w),
+              color:  const Color(0xfff5f5f5),
+            ),
+            height: 80.w,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
 
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+              SizedBox(width: 32.w,),
+              Image.asset(
+                'assets/images/3543@3x.png',
+                width: 20.w,
+                height: 20.w,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(width: 5.w,),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 18), // 默认样式
+                  children: [
+                    TextSpan(
+                      text: '今日待检测： ',
+                      style: TextStyle(color: Color(0xff999999),fontSize: 24.w),
+                    ),
+                    TextSpan(
+                      text: '999 ',
+                      style: TextStyle(color: Color(0xffff7f00),fontSize: 24.w),
+                    ),
+                  ],
+                ),
+              ),
 
-                      Expanded(
-                        child: Column(
+
+              SizedBox(width: 20.w,),
+              Image.asset(
+                'assets/images/3544@3x.png',
+                width: 20.w,
+                height: 20.w,
+                fit: BoxFit.fill,
+              ),
+              SizedBox(width: 5.w,),
+              RichText(
+                text: TextSpan(
+                  style: TextStyle(fontSize: 18), // 默认样式
+                  children: [
+                    TextSpan(
+                      text: '今日已检测： ',
+                      style: TextStyle(color: Color(0xff999999),fontSize: 24.w),
+                    ),
+                    TextSpan(
+                      text: '999 ',
+                      style: TextStyle(color: Color(0xff006cff),fontSize: 24.w),
+                    ),
+                  ],
+                ),
+              ),
+
+            ],),
+          ),
+          Expanded(
+            child: ListView.builder(
+                itemCount: 10,
+                padding: EdgeInsets.zero,
+                itemBuilder: (ctx,index){
+                  return Container(
+                    width: double.infinity,
+                    margin: EdgeInsets.only(left: 15,right: 15),
+                    height: 60,
+                    child: Stack(
+                      children: [
+
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
 
-                            Text('https://www.iconfont.cn/search/index?searchType=icon&g=%E7%AD%89%E5%BE%85&page=1&fromCollection=-1',
-                              style: TextStyle(color: Color(0xff006cff),fontSize: 12),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text('https://www.iconfont.cn/search/index?searchType=icon&g=%E7%AD%89%E5%BE%85&page=1&fromCollection=-1',
+                                    style: (index ==0||index ==1) ? TextStyle(color: Color(0xffff7f00),fontSize: 12):TextStyle(color: Color(0xff006cff),fontSize: 12),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+
+                                ],
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                              ),
                             ),
 
-                            const SizedBox(height: 4,),
+                            InkWell(
+                              onTap: (){
+                                Get.toNamed(AppRouter.checkResult);
+                              },
+                              child: Container(
+                                width: 70,
+                                height: 30,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                  color:(index ==0||index ==1) ?Color(0xffff7f00): Color(0xff006cff),
 
-                            Text('检测时间：2025-08-19 15:25:30',
-                              style: TextStyle(color: Colors.grey,fontSize: 12),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                                child: Text('检测完成',style: TextStyle(color: Color(0xffffffff) ,fontSize: 12),),
+                              ),
                             ),
 
                           ],
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
                         ),
-                      ),
 
-                      InkWell(
-                        onTap: (){
-                          Get.toNamed(AppRouter.checkResult);
-                        },
-                        child: Container(
-                          width: 70,
-                          height: 30,
-                          alignment: Alignment.center,
-                          decoration: BoxDecoration(
-                            border: BoxBorder.all(
-                              color: Color(0xff006cff),
-                            ),
-                            borderRadius: BorderRadius.circular(15),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            width: double.infinity,
+                            height: 0.1,
+                            color: Colors.grey,
                           ),
-                          child: Text('查看结果',style: TextStyle(color: Color(0xff006cff) ,fontSize: 12),),
                         ),
-                      ),
 
-                    ],
-                  ),
-
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Container(
-                      width: double.infinity,
-                      height: 0.1,
-                      color: Colors.grey,
+                      ],
                     ),
-                  ),
-
-                ],
-              ),
-            );
-          }),
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 
